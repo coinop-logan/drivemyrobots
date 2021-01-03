8be5bf9fdd22b0a9227b6952aa1f528cfdc6d4d3
coinop-logan <coinop.logan@gmail.com>
2021-01-03 08:10:53 +0000

    Can leave some vals unchanged in setraw

--- scripts/setraw.py
+++ scripts/setraw.py
@@ -6,13 +6,22 @@ def main():
         print("need more args")
         return
     
-    for i in range(len(sys.argv)):
-        int(i) # crash here if we can't convert all values
+    valsOrNone = []
+    for i in range(len(sys.argv) - 1):
+        arg = sys.argv[i + 1]
+        if arg.lower() == 'n':
+            valsOrNone.append(None)
+        else:
+            valsOrNone.append(int(arg))
     
     pwm = driver.makePwm()
     
-    for i in range(len(sys.argv) - 1):
-        val = int(sys.argv[i + 1])
+    for i in range(len(valsOrNone)):
+        val = valsOrNone[i]
+        
+        if val is None:
+            continue
+
         pwm.setServoPulse(i, val)
 
 main()
