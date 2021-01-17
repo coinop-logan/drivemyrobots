4dad17702b9bc511098497c7da9fc7490423e27f
coinop-logan <coinop.logan@gmail.com>
2021-01-17 11:44:19 +0000

    Rename for clarity

--- client/sayhi.py
+++ client/sayhi.py
@@ -1,10 +1,10 @@
 import socket
 
 server_ip = "34.228.70.168"
-server_port = 4546
+hello_port = 4546
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
-sock.sendto(b"Hi!", (server_ip, server_port))
+sock.sendto(b"Hi!", (server_ip, hello_port))
 
-print("sent")
+print("sent, listening")
--- server/hearhi.py
+++ server/hearhi.py
@@ -1,11 +1,10 @@
 import socket
 
-server_ip = "0.0.0.0"
-server_port = 4546
+hello_port = 4546
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
-sock.bind((server_ip, server_port))
+sock.bind(("0.0.0.0", hello_port))
 
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
