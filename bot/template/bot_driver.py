3556654f6133e84357449fd7a498fc9a36d4f38a
coinop-logan <coinop.logan@gmail.com>
2024-11-18 18:59:09 +0200

    more organization and notes

---
+++ bot/template/bot_driver.py
@@ -0,0 +1,16 @@
+from time import sleep
+
+botID = BOT_NAME_STRING
+
+def setup():
+    # initialize whatever
+    pass
+
+def close():
+    # clean up
+    pass
+
+def enactInput(inputState):
+    # do something to the bot to enact the inputState json data
+    pass
+
