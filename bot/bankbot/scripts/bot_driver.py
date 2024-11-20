28303e3fa8db2332c4036939e7e835a7f24521a1
coinop-logan <coinop.logan@gmail.com>
2024-11-20 18:49:15 +0200

    tweakin

--- bot/bankbot/scripts/bot_driver.py
+++ bot/bankbot/scripts/bot_driver.py
@@ -4,11 +4,19 @@ import setraw
 
 botID = "bankbot"
 
+SERVO_LOW_VAL = 10
+SERVO_HIGH_VAL = 4095
+
 def wheelValIsValid(wheelVal):
     try:
-        return wheelVal >= 0 and wheelVal < 4096
+        return wheelVal >= 0 and wheelVal <= 1
     except:
         return False
+
+def translateVal(inputVal):
+    translated = inputVal * (SERVO_HIGH_VAL - SERVO_LOW_VAL) + SERVO_LOW_VAL
+    print(translated)
+    return translated
 
 def setup():
     pass
@@ -18,6 +26,6 @@ def close():
 
 def enactInput(inputState):
     if not (wheelValIsValid(inputState['left_wheel']) and wheelValIsValid(inputState['right_wheel'])):
-        raise ValueError("wheel vals out of acceptable range")
-        
-    setraw.setRaw([inputState['left_wheel'], inputState['right_wheel']])
+        raise ValueError("wheel vals out of acceptable range ({SERVO_LOW_VAL} - {SERVO_HIGH_VAL})")
+    
+    setraw.setRaw([translateVal(inputState['left_wheel']), translateVal(inputState['right_wheel'])])
